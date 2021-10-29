9bac5fa92d08544b058732fa3f92ae918f3d0ff8
AlexandrMeyer <alexandrmeyerr@gmail.com>
2021-10-29 10:31:43 +0300

    Initial Commit

---
+++ ColorMixSwiftUI/ColorMixSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixSwiftUIApp.swift
+//  ColorMixSwiftUI
+//
+//  Created by Александр on 29.10.21.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
